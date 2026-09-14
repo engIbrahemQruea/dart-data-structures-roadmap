100768dce7e0cb0059a9ae80bec37f3021d796ef
engIbrahemQruea <mmany6186@gmail.com>
2026-09-14 20:57:03 +0300

    feat: Initialize level2 structure with README, changelog, and basic files

---
+++ level2/bin/level2.dart
@@ -0,0 +1,5 @@
+import 'package:level2/level2.dart' as level2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${level2.calculate()}!');
+}
---
+++ level2/test/level2_test.dart
@@ -0,0 +1,8 @@
+import 'package:level2/level2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
